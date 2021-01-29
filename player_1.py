b3c087071a4f95120998d41d4c4fb86d2616d049
Colin Kelleher <117303363@umail.ucc.ie>
2021-01-29 20:58:28 +0000

    Initialiation of connection

--- player_1.py
+++ player_1.py
@@ -1,3 +1,6 @@
+import socket
+import sys
+
 class Client():
     def __init__(self, username=''):
         self.username = username
@@ -5,4 +8,19 @@ class Client():
     def inputUsername(self):
         self.username = input("Please enter your username: ")
 
-    
+    def connectionInitialisation(self):
+        host= '127.0.0.1'
+        port= 1234
+
+        # Socket object creation
+        try:
+            self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error:
+            sys.exit()
+
+        # Connection to port
+        try:
+            self.server_socket.connect((host, port))
+        except socket.error:
+            sys.exit()
+
