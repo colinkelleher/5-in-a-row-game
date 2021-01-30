5874fde5dec067d0815350955a021c619a12afaf
Colin Kelleher <117303363@umail.ucc.ie>
2021-01-30 19:33:54 +0000

    Getting PlayerMove

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import sys
 import socket
+import threading
 from threading import *
+import json
 
 
 class Server():
@@ -15,6 +17,8 @@ class Server():
         self.grid = kwargs.get("grid",[])
         self.disc = kwargs.get("disc", {})
         self.lock = RLock()
+        self.playerGo = kwargs.get("playerGo", "")
+        self.playerWaiting = kwargs.get("playerWaiting", "")
 
 
     def newPlayer(self,player):
@@ -27,36 +31,64 @@ class Server():
         else:
             return False
 
+    def getPlayerMove(self): # Switch between players
+        if self.playerGo == self.players[0]:
+            self.playerGo = self.players[1]
+            self.playerWaiting =self.players[0]
+        else:
+            self.playerGo=self.players[0]
+            self.playerWaiting  =self.players[1]
+
+        jsonReady = {"status": "READY","grid": self.grid,"disc": self.disc[self.playerGo]}
+        readyResponse = json.dumps(jsonReady)
+        self.players[self.playerGo].send(readyResponse.encode())
+
+        jsonWait = {"status":"WAIT", "grid":self.grid, "disc": self.disc[self.playerWaiting]}
+        waitResponse = json.dumps(jsonWait)
+        self.players[self.playerWaiting].send(waitResponse.encode())
+
+
+
     def createGrid(self):
         for number in range(self.rows):
             self.grid.append(["[ ]"] * self.columns)
 
-    def connectToPlayers(self, playerConnection):
+    def checkResult(self):
+        '''
+        Check to see if there is 5 in a row
+        '''
+        sys.exit()
+
+    def placeDisc(self, position):
+        position = position - 1
+        gridPosition = False
+
         sys.exit()
 
 
+    def connectToPlayers(self, playerConnection):
+        sys.exit()
+
+    def playerConnection(self):
+        sys.exit()
 
     def connectionInitialisation(self):
         host= '127.0.0.1'
         port= 1234
-
         # Socket object creation
         try:
             self.client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         except socket.error:
             sys.exit()
-
         # Connection to port
         try:
             self.client_socket.bind((host,port))
         except socket.error:
             sys.exit()
-
         self.client_socket.listen(5)
 
     def runServer(self):
-        sys.exit()
-
+        self.serverSetup()
 
 
 if __name__ == "__main__":
