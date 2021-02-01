f82b94afa5a5c4a5240c8d7cadf0514cd13c0fcb
Colin Kelleher <117303363@umail.ucc.ie>
2021-02-01 17:25:17 +0000

    Some tests
    Signed-off-by: Colin Kelleher <117303363@umail.ucc.ie>

--- testing/test.py
+++ testing/test.py
@@ -3,7 +3,7 @@ from player_1 import Client
 import unittest
 
 
-class testSetup (unittest.TestCase):
+class testPlayer (unittest.TestCase):
 
     def test_newPlayer(self):
         server = Server()
@@ -14,6 +14,9 @@ class testSetup (unittest.TestCase):
         server = Server()
         server.newPlayer("Colin")
         self.assertFalse(server.players == ["Genesys"])
+
+class testgrid(unittest.TestCase):
+
 
     def test_GridSetup(self):
         server = Server()
@@ -39,11 +42,30 @@ class testSetup (unittest.TestCase):
         ['[ ]', '[ ]', '[ ]', '[ ]']
     ])
 
+class testResults(unittest.TestCase):
+    def test_Draw(self):
+        server = Server()
+        server.kwargs={"playerGo": "Colin","disc": {"Colin": "O"},"grid": [
+                    ['[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'],
+                    ['[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'],
+                    ['[X]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'],
+                    ['[X]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'],
+                    ['[X]', '[ ]', '[ ]', '[O]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'],
+                    ['[X]', '[ ]', '[ ]', '[O]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]']
+                ]
+            }
+        self.assertFalse(server.isDraw == False)
 
-
+class ClientTests(unittest.TestCase):
+    def test_username(self):
+        client = Client()
+        username = client.inputUsername()
+        self.assertFalse(username== "jk")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    testPlayer()
+    testgrid()
+    testResults()
 
 
